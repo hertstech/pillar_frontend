7d25c59a992f5bd624ff9bf2f20da98074395fc7
ASAP_A1 <aaronrickymeek@gmail.com>
2024-11-14 16:09:50 +0100

    fix: :bug: corrected errors preventing signup

--- src/Guard/AuthGuard.tsx
+++ src/Guard/AuthGuard.tsx
@@ -1,32 +1,33 @@
 import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { dispatchLogout } from "../redux/userSlice";
-import { Navigate } from "react-router-dom";
 
 export default function AuthGuard({ children }: { children: React.ReactNode }) {
   const isLogged = useSelector((state: any) => state.user.isLogged);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
-    if (isLogged === true) {
-      <Navigate to="/dashboard/home" />;
-    }
+    const isRegisterRoute = location.pathname === "/auth/register";
 
-    if (!isLogged) {
+    if (isLogged && !isRegisterRoute) {
+      navigate("/dashboard/home");
+    } else if (!isLogged && !isRegisterRoute) {
       dispatch(dispatchLogout());
       navigate("/auth/login");
     }
 
     const timer = setTimeout(() => {
-      if (!isLogged) {
+      if (!isLogged && !isRegisterRoute) {
         dispatch(dispatchLogout());
         navigate("/auth/login");
       }
     }, 2 * 60 * 1000);
+
     return () => clearTimeout(timer);
-  }, [isLogged, dispatch, navigate]);
+  }, [isLogged, dispatch, navigate, location.pathname]);
 
   return <>{children}</>;
 }
