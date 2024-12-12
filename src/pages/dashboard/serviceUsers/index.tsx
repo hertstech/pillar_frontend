256fa08b70363d76175c8ffd1fc7e36e1b7b6944
ASAP_A1 <aaronrickymeek@gmail.com>
2024-12-12 19:13:28 +0100

    chore: update access right for sharing or requesting

--- src/pages/dashboard/serviceUsers/index.tsx
+++ src/pages/dashboard/serviceUsers/index.tsx
@@ -224,7 +224,7 @@ export default function Singleuser() {
           <PopperOver
             position="bottom-end"
             popperContent={
-              writeAccess ? (
+              !writeAccess ? (
                 <Box className="bg-white min-w-[200px] max-h-[112px] rounded-lg border-t">
                   <button
                     onClick={() => setIsOpenRequestForm(true)}
