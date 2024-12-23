e68d943c83d20584c6720440e5c4d9305a93624e
ASAP_A1 <aaronrickymeek@gmail.com>
2024-12-23 19:29:59 +0100

    chore: update test types value for server

--- src/pages/dashboard/serviceUsers/Test/TestDetails/Results.tsx
+++ src/pages/dashboard/serviceUsers/Test/TestDetails/Results.tsx
@@ -17,12 +17,36 @@ interface Result {
   test_types: string;
   date: string;
   reading: string;
-  unit: string;
 }
 
 interface IProps {
   data: Result[];
 }
+
+const testUnits: Record<string, string> = {
+  hemoglobin: "g/dL",
+  wbc: "10³/μL",
+  rbc: "10⁶/μL",
+  platelets: "10³/μL",
+  haematocrit: "%",
+  corpuscularVol: "fL",
+  corpuscularHb: "pg",
+  bloodGlucoseFasting: "mg/dL",
+  bloodGlucoseRandom: "mg/dL",
+  bloodGlucoseGlycatedHaemoglobin: "%",
+  cholesterol: "mg/dL",
+  alt: "U/L",
+  ast: "U/L",
+  bilirubinTotal: "mg/dL",
+  alp: "U/L",
+  albumin: "g/dL",
+  ggt: "U/L",
+  creatinine: "mg/dL",
+  glr: "mL/min/1.73m²",
+  urea: "mg/dL",
+  tsh: "μIU/mL",
+  t4: "ng/dL",
+};
 
 export const Results = ({ data }: IProps) => {
   const [expanded, setExpanded] = useState<number | false>(false);
@@ -38,60 +62,64 @@ export const Results = ({ data }: IProps) => {
       {!data || data.length === 0 ? (
         <span>No test added </span>
       ) : (
-        data?.map((result) => (
-          <Accordion
-            key={result.id}
-            expanded={expanded === result.id}
-            onChange={handleAccordionChange(result.id)}
-            className="flex flex-col justify-center gap-8  !rounded-xl !border
-        !border-neu-50 !shadow-none !min-h-[84px]"
-            sx={{
-              "&::before": {
-                backgroundColor: "transparent",
-              },
-            }}
-          >
-            <AccordionSummary
-              expandIcon={
-                <FaAngleDown
-                  className={classNames(
-                    "absolute top-5 right-2 transition-transform duration-300",
-                    expanded ? "!top-4 !left-2" : "rotate-0"
-                  )}
-                />
-              }
-              className="!max-h-[52px] !mt-7 !shadow-none"
+        data?.map((result) => {
+          const unit = testUnits[result.test_types] || ""; 
+
+          return (
+            <Accordion
+              key={result.id}
+              expanded={expanded === result.id}
+              onChange={handleAccordionChange(result.id)}
+              className="flex flex-col justify-center gap-8  !rounded-xl !border
+          !border-neu-50 !shadow-none !min-h-[84px]"
+              sx={{
+                "&::before": {
+                  backgroundColor: "transparent",
+                },
+              }}
             >
-              <Box className="flex flex-col w-full gap-8">
-                <p className="flex items-center gap-2 text-sm text-neu-500 font-normal capitalize">
-                  {icons[result.category]}{" "}
-                  {getNameByValue(result.category, testData.category)} •{" "}
-                  {result.date}
-                </p>
-                <Box className="flex justify-between items-center w-[95%]">
-                  <p className="text-base font-semibold capitalize">
-                    {getNameByValue(result.test_types, testData.category)}
+              <AccordionSummary
+                expandIcon={
+                  <FaAngleDown
+                    className={classNames(
+                      "absolute top-5 right-2 transition-transform duration-300",
+                      expanded ? "!top-4 !left-2" : "rotate-0"
+                    )}
+                  />
+                }
+                className="!max-h-[52px] !mt-7 !shadow-none"
+              >
+                <Box className="flex flex-col w-full gap-8">
+                  <p className="flex items-center gap-2 text-sm text-neu-500 font-normal capitalize">
+                    {icons[result.category]}{" "}
+                    {getNameByValue(result.category, testData.category)} •{" "}
+                    {result.date}
                   </p>
-                  <p className="text-base text-succ font-semibold mr-2">
-                    {result.reading}{" "}
-                    <span
-                      className={classNames(
-                        "text-sm font-normal ",
-                        unitColors[result.unit] || "text-neu-400"
-                      )}
-                    >
-                      {result.unit}
-                    </span>
-                  </p>
+                  <Box className="flex justify-between items-center w-[95%]">
+                    <p className="text-base font-semibold capitalize">
+                      {getNameByValue(result.test_types, testData.category)}
+                    </p>
+                    <p className="text-base text-succ font-semibold mr-2">
+                      {result.reading}{" "}
+                      <span
+                        className={classNames(
+                          "text-sm font-normal",
+                          unitColors[unit] || "text-neu-400"
+                        )}
+                      >
+                        {unit}
+                      </span>
+                    </p>
+                  </Box>
                 </Box>
-              </Box>
-            </AccordionSummary>
-            <AccordionDetails className="!border-none !shadow-none">
-              Additional details for{" "}
-              {getNameByValue(result.test_types, testData.category)}.
-            </AccordionDetails>
-          </Accordion>
-        ))
+              </AccordionSummary>
+              <AccordionDetails className="!border-none !shadow-none">
+                Additional details for{" "}
+                {getNameByValue(result.test_types, testData.category)}.
+              </AccordionDetails>
+            </Accordion>
+          );
+        })
       )}
     </div>
   );
